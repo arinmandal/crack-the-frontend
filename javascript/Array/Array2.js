ea2f25d76de95088e6ce29904839bba8866a8835
Arin <arinmandal40@gmail.com>
2025-06-27 23:04:35 +0530

    Array methods: includes(), sort(), split(), join()

--- javascript/Array/Array2.js
+++ javascript/Array/Array2.js
@@ -3,7 +3,7 @@
 /* Array push() Method
 
 The arr.push() method is used to push one or more values into the array. This method changes the length of the array by the number of elements added to the array. */
-
+/* 
 function arrPush() {
   // original array
   let arr = ["GFG", "ARR", "KGF"];
@@ -11,12 +11,12 @@ function arrPush() {
   arr.push("ROG");
   console.log(arr);
 }
-arrPush();
+arrPush(); */
 
 /* Array pop() Method
 The arr.pop() method is used to remove the last element of the array and also returns the removed element. This function decreases the length of the array. */
 
-function arrPop() {
+/* function arrPop() {
   // original array
   let arr = ["cream", "ice", "pasta", "sauce"];
 
@@ -24,13 +24,13 @@ function arrPop() {
   arr.pop();
   console.log(arr);
 }
-arrPop();
+arrPop(); */
 
 /* Array slice() Method
 
 The arr. slice() method returns a new array containing a portion of the array on which it is implemented. The original remains unchanged. */
 
-function func() {
+/* function func() {
   // Original Array
   var arr = [23, 56, 87, 32, 75, 13];
   // Extracted array
@@ -39,4 +39,50 @@ function func() {
   console.log("<br>");
   console.log(new_arr);
 }
-func();
+func(); */
+
+//! Array includes() Method
+
+// The array.includes() method is used to know either a particular element is present in the array or not and accordingly, it returns true or false i.e, if the element is present, then it returns true otherwise false.
+
+/* 
+let names = ["one", "two", "three", "four"]
+console.log(names.includes("four")) */
+
+//! Array sort() Method
+
+/* The arr.sort() method is used to sort the array in place in a given order according to the compare() function. If the method is omitted then the array is sorted in ascending order. */
+
+/* 
+let num = [4, 2, 8, 6, 8, 7, 5, 2];
+console.log(num.sort()); // [2,2,4,5,6,7,8,8] */
+
+// let str = ["blue", "green", "ranbow", "Amber", "crimson"];
+// console.log(str.sort()) //[("Amber", "blue", "crimson", "green", "ranbow")];
+
+
+//! Array split and join method
+
+/* split Method
+The split method splits a string into an array of substrings based on a specified separator. */
+
+//? Split Method
+// The split method splits a string into an array of substrings based on a specified separator.
+// split() splits a string into an array of substrings, and returns the array:
+// Syntax:
+// str.split(separator, limit)
+
+/* let str = 'hello god plans'
+console.log(str.split(" ")) //[ 'hello', 'god', 'plans' ]
+ */
+
+
+// ? Array Join()
+/* The join() method returns an array as a string.
+The join() method does not change the original array.
+Any separator can be specified.The default is comma(,). */
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.join(",");
+// console.log(text)
