8e737174d93c4ea3079488cbf9bd206f726fa5cf
Arin <arinmandal40@gmail.com>
2025-07-09 23:56:22 +0530

    docs: revise JS fundamentals on DOM access

---
+++ javascript/JavaScript - Learn Everything/DOM/script.js
@@ -0,0 +1,24 @@
+//! document.getElementById, document.getElementsByClassName, document.querySelector, document.querySelectorAll
+
+// Todo: Accessing Elements in the DOM
+// ? document.getElementById
+
+/* let abc = document.getElementById("abc");
+console.dir(abc); */
+
+// ? document.getElementsByClassName
+
+/* let abc = document.getElementsByClassName("abc");
+console.log(abc);
+ */
+
+//? document.querySelector
+
+/* let abc = document.querySelector("h1");
+console.log(abc);
+ */
+//? document.querySelectorAll
+
+/* let abc = document.querySelectorAll("h1");
+console.log(abc); */
+
