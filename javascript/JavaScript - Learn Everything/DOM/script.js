671ac34a9fe63109a8d4453bb4a09193f3230138
Arin <arinmandal40@gmail.com>
2025-07-10 00:08:10 +0530

    feat: add examples for attribute manipulation (setAttribute, getAttribute, removeAttribute)

--- javascript/JavaScript - Learn Everything/DOM/script.js
+++ javascript/JavaScript - Learn Everything/DOM/script.js
@@ -32,3 +32,21 @@ let h1 = document.querySelector("h1");
 //? change HTML
 /* h1.innerHTML = "<strong>Hello World!</strong>";
 console.dir(h1); */
+
+// Todo: Attribute Manipulation
+// let a = document.querySelector("a");
+// a.href = "https://www.google.com";
+
+// setAttribute
+/* a.setAttribute("href", "https://www.google.com");
+let img = document.querySelector("img");
+img.setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1750785328656-eb4c9942e58f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+); */
+
+//? getAttribute
+// console.log(a.getAttribute("href"));
+
+// ? removeAttribute
+// img.removeAttribute("src");
